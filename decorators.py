826a030b117ab73ef3a6f001bb0f391d571b0275
Adam Cox <adamcox.dev@gmail.com>
2023-01-11 20:12:30 -0700

    saving progress

--- decorators.py
+++ decorators.py
@@ -17,3 +17,47 @@ def add_five(num):
 # add_two(7) can't use function outside of add_five           
               
 add_five(10)
+
+# 3. Returning functions from functions
+ 
+def get_math_function(operation): # + or - 
+    def add(num1, num2):
+        return num1 + num2
+    def sub(num1 - num2):
+        return num1 - num2
+    
+    if operation == '+':
+       return add
+    elif operation == '-':
+         return sub
+              
+add_function = get_math_funtion('+')
+sub_function = get_math_function('-')
+
+# 4. Decorating a function
+              
+def title_decorator(print_name_function):
+    def wrapper():
+        print("Professor:")
+        print_name_function()
+    return wrapper
+              
+def print_my_name():
+    print("Adam")
+              
+              
+decorated_function = title_decorator(print_my_name) 
+              
+decorated_function()
+              
+              
+              
+              
+              
+              
+              
+              
+              
+              
+              
+              
