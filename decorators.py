e208358ccc8b454193e78e0cba9a7a37a6daea7d
Adam Cox <adamcox.dev@gmail.com>
2023-01-11 19:58:45 -0700

    practicing and understanding decorators

--- decorators.py
+++ decorators.py
@@ -1,1 +1,19 @@
+# 1. Functions are objects
 
+def add_five(num):
+  print(num + 5)
+  
+add_five(2)
+
+# 2. Functions within functions
+
+def add_five(num):
+  def add_two(num:
+      return num +2
+              
+  num_plus_two = add_two(num)
+  print(num_plus_two + 3)
+
+# add_two(7) can't use function outside of add_five           
+              
+add_five(10)
